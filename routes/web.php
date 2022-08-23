fcdca12bc85a7a46628a717b956251f4cb9e3b11
lehuy2709 <lvhlehuy2709@gmail.com>
2022-08-23 14:35:28 +0700

    back up

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FacultyController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +20,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard.index');
 });
+
 Route::resources([
     'faculties' => FacultyController::class,
     'subjects' => SubjectController::class,
     'students'=> StudentController::class,
 ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
